b1125c328e071a91c8f04215c06316ad176bd2bb
danijoke <40579556+fd4n9@users.noreply.github.com>
2020-04-26 08:23:17 +0300

    removed machineHas()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -47,19 +47,6 @@ public class Main {
 
     }
 
-    static void machineHas() {
-
-        System.out.println("The coffee machine has:");
-        System.out.println(water + " of water");
-        System.out.println(milk + " of milk");
-        System.out.println(beans + " of coffee beans");
-        System.out.println(cups + " of disposable cups");
-        System.out.println(money + " of money");
-        System.out.println();
-        System.out.print("Write action (buy, fill, take): ");
-
-    }
-
     static void buy () {
         System.out.print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
         System.out.println();
